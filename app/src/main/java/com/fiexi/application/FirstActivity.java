9c5f388f8473ed1e8865d0196cc8323c09c92430
叶帆 <fan_ye@whirlpool-china.com>
2019-11-26 10:42:41 +0800

    Initial commit

---
+++ app/src/main/java/com/fiexi/application/FirstActivity.java
@@ -0,0 +1,12 @@
+package com.fiexi.application;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
